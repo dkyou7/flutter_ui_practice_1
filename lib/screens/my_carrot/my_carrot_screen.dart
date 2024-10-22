093a23dd3f896f1415e05dc706cd18b31e48759c
You Dong Kwan <dkyou7@naver.com>
2024-10-22 17:59:19 +0900

    내 정보 초기 UI 작성

--- lib/screens/my_carrot/my_carrot_screen.dart
+++ lib/screens/my_carrot/my_carrot_screen.dart
@@ -1,12 +1,33 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class MyCarrotScreen extends StatelessWidget {
   const MyCarrotScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('My Carrot Screen'),
+    return Scaffold(
+      backgroundColor: Colors.grey[100],
+      appBar: AppBar(
+        title: const Text('내정보'),
+        actions: [
+          IconButton(onPressed: () {}, icon: const Icon(Icons.settings)),
+        ],
+        bottom: const PreferredSize(
+          preferredSize: Size.fromHeight(0.5),
+          child: Divider(
+            thickness: 0.5,
+            height: 0.5,
+            color: Colors.grey,
+          ),
+        ),
+      ),
+      body: ListView(
+        children: [
+          //header
+          //iconmenu
+        ],
+      ),
     );
   }
 }
