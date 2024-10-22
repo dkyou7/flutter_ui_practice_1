b29f2ea76c7cefded46545ffb06e3a01439bd3b7
You Dong Kwan <dkyou7@naver.com>
2024-10-22 14:40:14 +0900

    HomeScreen Widget 완성하기

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,12 +1,49 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Home Screen'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Row(
+          children: [
+            Text('좌동'),
+            SizedBox(
+              width: 4.0,
+            ),
+            Icon(
+              CupertinoIcons.chevron_down,
+              size: 15.0,
+            ),
+          ],
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(CupertinoIcons.search),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(CupertinoIcons.list_dash),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(CupertinoIcons.bell),
+          ),
+        ],
+        bottom: const PreferredSize(
+          preferredSize: Size.fromHeight(0.5),
+          child: Divider(
+            thickness: 0.5,
+            height: 0.5,
+            color: Colors.grey,
+          ),
+        ),
+      ),
+      body: Container(),
     );
   }
 }
