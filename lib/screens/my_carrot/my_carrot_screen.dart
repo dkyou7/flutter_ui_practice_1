9a7cd6d8e7c016ce4a4efa82bba110183c2987a8
You Dong Kwan <dkyou7@naver.com>
2024-10-22 14:07:24 +0900

    MainScreens Widget 완성하기

---
+++ lib/screens/my_carrot/my_carrot_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
+class MyCarrotScreen extends StatelessWidget {
+  const MyCarrotScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('My Carrot Screen'),
+    );
+  }
+}
---
+++ lib/screens/near_me/near_me_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
+class NearMeScreen extends StatelessWidget {
+  const NearMeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: const Text('near me'),
+    );
+  }
+}
